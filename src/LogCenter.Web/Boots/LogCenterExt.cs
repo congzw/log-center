37cfddc73d1cc62133f014967aa9eea5d9383e3f
congzw <2981810300@qq.com>
2020-07-15 11:08:17 +0800

    refact HttpRequestServiceLocator

--- src/LogCenter.Web/Boots/LogCenterExt.cs
+++ src/LogCenter.Web/Boots/LogCenterExt.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using LogCenter.Common;
+﻿using LogCenter.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
 
 namespace LogCenter.Web.Boots
 {
